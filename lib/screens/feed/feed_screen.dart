c06e2f0c9e2c89074926416baf1a8914ee79404c
adeelzahid1 <developer.adeelzahid@gmail.com>
2022-05-07 09:29:51 +0500

    cloud functions added

--- lib/screens/feed/feed_screen.dart
+++ lib/screens/feed/feed_screen.dart
@@ -14,52 +14,64 @@ class FeedScreen extends StatefulWidget {
 class _FeedScreenState extends State<FeedScreen> {
   @override
   Widget build(BuildContext context) {
-    return BlocConsumer<FeedBloc, FeedState>(
-      listener: (context, state) {
-        if (state.status == FeedStatus.error) {
-          showDialog(
-            context: context,
-            builder: (context) => ErrorDialog(e: state.failure),
-          );
-        }
-      },
-      builder: (context, state) {
-        return Scaffold(
-          appBar: AppBar(
-            title: const Text('Instagram'),
-            actions: [
-              if (state.posts.isEmpty && state.status == FeedStatus.loaded)
-                IconButton(
-                  icon: const Icon(Icons.refresh),
-                  onPressed: () =>
-                      context.read<FeedBloc>().add(FeedFetchPosts()),
-                )
-            ],
-          ),
-          body: _buildBody(state),
-        );
-      },
+    return Scaffold(
+        appBar: AppBar(
+        title: Text('Feeb / Home'),
+      ),
+      body: Center(child: Text('Home Feed'),),
     );
   }
-
-    Widget _buildBody(FeedState state) {
-    switch (state.status) {
-      case FeedStatus.loading:
-        return const Center(child: CircularProgressIndicator());
-      default:
-        return RefreshIndicator(
-          onRefresh: () async {
-            context.read<FeedBloc>().add(FeedFetchPosts());
-            // return true;
-          },
-          child: ListView.builder(
-            itemCount: state.posts.length,
-            itemBuilder: (BuildContext context, int index) {
-              final post = state.posts[index];
-              return PostView(post: post, isLiked: false);
-            },
-          ),
-        );
-    }
-  }
 }
+
+// class _FeedScreenState extends State<FeedScreen> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return BlocConsumer<FeedBloc, FeedState>(
+//       listener: (context, state) {
+//         if (state.status == FeedStatus.error) {
+//           showDialog(
+//             context: context,
+//             builder: (context) => ErrorDialog(e: state.failure),
+//           );
+//         }
+//       },
+//       builder: (context, state) {
+//         return Scaffold(
+//           appBar: AppBar(
+//             title: const Text('Instagram'),
+//             actions: [
+//               if (state.posts.isEmpty && state.status == FeedStatus.loaded)
+//                 IconButton(
+//                   icon: const Icon(Icons.refresh),
+//                   onPressed: () =>
+//                       context.read<FeedBloc>().add(FeedFetchPosts()),
+//                 )
+//             ],
+//           ),
+//           body: _buildBody(state),
+//         );
+//       },
+//     );
+//   }
+
+//     Widget _buildBody(FeedState state) {
+//     switch (state.status) {
+//       case FeedStatus.loading:
+//         return const Center(child: CircularProgressIndicator());
+//       default:
+//         return RefreshIndicator(
+//           onRefresh: () async {
+//             context.read<FeedBloc>().add(FeedFetchPosts());
+//             // return true;
+//           },
+//           child: ListView.builder(
+//             itemCount: state.posts.length,
+//             itemBuilder: (BuildContext context, int index) {
+//               final post = state.posts[index];
+//               return PostView(post: post, isLiked: false);
+//             },
+//           ),
+//         );
+//     }
+//   }
+// }
